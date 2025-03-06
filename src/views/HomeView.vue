48520bd2a90575e886c3f6a7fb1042a388e3a6e6
“Aiden <adnrdmnd@gmail.com>
2025-03-06 10:35:44 -0500

    rename index content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,11 @@
   <main class="flex flex-col flex-1 items-center justify-center">
     <Transition
       appear
-      v-for="(text, index) in elements"
+      v-for="(tag, index) in elements"
       :key="index"
       :style="{ '--delay': `${index * 1250}ms` }"
     >
-      <span v-html="text"></span>
+      <span v-html="tag"></span>
     </Transition>
   </main>
 </template>
@@ -16,7 +16,7 @@ const elements = [
   "<h1 class='text-2xl'>hi, i'm aiden.</h1>",
   '<h1 class="text-2xl">this website is split in half.</h1>',
   '<h1 class="text-2xl mb-8">check out the navbar ↑</h1>',
-  '<div class="flex gap-3"><a href="mailto:aidenredmondd@gmail.com" class="underline underline-offset-[6px] hover:text-pink">aidenredmondd@gmail.com</a>|<a href="https://github.com/aredmondd" target="_blank" class="underline underline-offset-[6px] hover:text-pink">github/aredmondd</a></div>',
+  '<div class="text-sm flex gap-2"><a href="mailto:aidenredmondd@gmail.com" class="underline underline-offset-[6px] hover:text-pink">aidenredmondd@gmail.com</a>|<a href="https://github.com/aredmondd" target="_blank" class="underline underline-offset-[6px] hover:text-pink">github/aredmondd</a></div>',
 ]
 </script>
 
