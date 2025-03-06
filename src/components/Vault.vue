673c60f86dd0566adafe7a440275e857685b65de
“Aiden <adnrdmnd@gmail.com>
2025-03-06 14:28:42 -0500

    basic vault complete

---
+++ src/components/Vault.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const raindrops = ref([])
+const error = ref(null)
+
+async function fetchRaindrops() {
+  const API_URL = 'https://api.raindrop.io/rest/v1/raindrops/50543962'
+  const API_TOKEN = import.meta.env.VITE_RAINDROP_ACCESS_TOKEN
+
+  try {
+    const response = await axios.get(API_URL, {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    })
+
+    raindrops.value = response.data.items
+  } catch (err) {
+    console.error(err)
+    error.value = 'Failed to fetch raindrops. Please try again.'
+  }
+}
+
+function generateDate(date) {
+  let month = date.substring(date.indexOf('-') + 1, date.indexOf('-', 5))
+  let day = date.substring(date.indexOf('-', 5) + 1, date.indexOf('T'))
+  let year = date.substring(0, date.indexOf('-'))
+
+  return year + '-' + month + '-' + day
+}
+
+onMounted(() => {
+  fetchRaindrops()
+})
+</script>
+
+<template>
+  <div>
+    <div v-if="error" class="text-red-500">{{ error }}</div>
+    <ul>
+      <li v-for="raindrop in raindrops" :key="raindrop._id" class="flex items-end">
+        <!-- Left: Title -->
+        <a v-bind:href="raindrop.link" target="_blank" class="hover:text-pink truncate">
+          {{ raindrop.title }}
+        </a>
+
+        <!-- Middle: Dots -->
+        <span
+          class="flex-grow mx-2 border-b-2 border-dotted border-black border-opacity-50 mb-[6px]"
+        ></span>
+
+        <!-- Right: Date -->
+        <span class="text-black whitespace-nowrap font-mono">
+          {{ generateDate(raindrop.created) }}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
--- src/views/UnofficiallyView.vue
+++ src/views/UnofficiallyView.vue
@@ -1,14 +1,29 @@
 <script setup>
 import ContentScroller from '@/components/ContentScroller.vue'
+import Vault from '@/components/Vault.vue'
 </script>
 
 <template>
   <div class="flex flex-col flex-1">
     <!-- what i'm loving section -->
     <h2 class="text-4xl font-bold mt-12 mb-3">what i'm loving 💚</h2>
-    <h3>a huge part of my inspiration comes from media. <br />here's what i'm into right now.</h3>
-    <ContentScroller />
+    <h3 class="mb-6">
+      a huge part of my inspiration comes from media. <br />here's what i'm into right now.
+    </h3>
+    <ContentScroller class="mb-12" />
 
     <!-- raindrop section -->
+    <h2 class="text-4xl font-bold mt-12 mb-3">the vault 🏛️</h2>
+    <h3 class="mb-6">
+      this section is <span class="line-through">stolen from</span> inspired by
+      <a
+        href="https://www.iamrob.in/bookmarks"
+        target="_blank"
+        class="underline underline-offset-4 hover:text-pink transition-all duration-300 ease-in-out"
+        >iamrobin's bookmarks &#x2197;</a
+      >. <br />
+      here are some of my favorite websites, tools, & more.
+    </h3>
+    <Vault class="mb-12" />
   </div>
 </template>
