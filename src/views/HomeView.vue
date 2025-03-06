10ce03a75ddb3a3a2375696f6f00750c2304b712
“Aiden <adnrdmnd@gmail.com>
2025-03-05 20:05:13 -0500

    inter + styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-6xl">hello there</h1>
 </template>
