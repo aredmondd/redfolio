14491364264bcb4ca208e6788d240158872add87
“Aiden <adnrdmnd@gmail.com>
2025-03-06 19:35:23 -0500

    top three repos

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,12 +4,18 @@ import { ref, onMounted } from 'vue'
 let github_url = 'https://api.github.com/users/aredmondd/repos'
 
 let repos = ref([])
+let bestthree = ref([])
 
 async function fetchRepos() {
   try {
     const response = await fetch(github_url)
     repos.value = await response.json()
-    console.log(repos)
+    repos.value.forEach((repo) => {
+      if (repo.name == 'perfect-kana' || repo.name == 'redfolio-v2' || repo.name == 'stridespace') {
+        console.log(repo)
+        bestthree.value.push(repo)
+      }
+    })
   } catch (error) {
     console.error('Error:', error)
   }
@@ -21,7 +27,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <ul>
-    <li v-for="repo in repos" :key="repo.id">{{ repo.name }}</li>
-  </ul>
+  <div class="grid grid-cols-3 mb-12 gap-6">
+    <div
+      v-for="repo in bestthree"
+      :key="repo.id"
+      class="border-2 rounded-lg py-12 flex flex-col items-center justify-center hover:border-pink hover:text-pink"
+    >
+      <a :href="repo.html_url" target="_blank" class="text-xl">{{ repo.name }}</a>
+      <p v-if="repo.description != null" class="text-xs">{{ repo.description }}</p>
+    </div>
+  </div>
 </template>
