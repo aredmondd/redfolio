a2ea1ffea33fd082a9112a8b01dbe298f25b0d5f
“Aiden <adnrdmnd@gmail.com>
2025-03-08 14:55:08 -0500

    404

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import OfficiallyView from '@/views/OfficiallyView.vue'
 import UnofficiallyView from '@/views/UnofficiallyView.vue'
+import FourOhFour from '@/views/FourOhFour.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,10 @@ const router = createRouter({
       path: '/unofficially',
       name: 'unofficially',
       component: UnofficiallyView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: FourOhFour,
     },
   ],
 })
---
+++ src/views/FourOhFour.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col flex-1 items-center">
+    <h1 class="text-4xl font-bold mt-36 mb-6">404</h1>
+    <p>
+      lost. you have found it. <br />
+      what? who knows. not me or you. <br />
+      you should just go back.
+    </p>
+  </div>
+</template>
