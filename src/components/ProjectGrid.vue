cca007b921305ee03e590a1b41bdbd654cb0e4b4
Aiden Redmond <adnrdmnd@gmail.com>
2025-06-15 15:54:48 -0400

    work is mobile friendly

--- src/components/ProjectGrid.vue
+++ src/components/ProjectGrid.vue
@@ -6,15 +6,16 @@ import NewProjectCard from './NewProjectCard.vue'
 <template>
   <Transition appear>
     <div class="space-y-48 sm:mt-12 sm:mx-4">
+      <!-- freelance -->
       <div class="flex flex-col gap-12">
-        <div class="mt-12 flex gap-12 justify-between">
+        <div class="mt-12 flex flex-col sm:flex-row gap-6 sm:gap-12 justify-between">
           <h2 class="text-left text-6xl font-bold">FREELANCE WORK</h2>
           <h3 class="text-right text-xs sm:text-base text-black/50">
             recently, i've been working as a freelance web developer. <br />
             click on a project to learn more.
           </h3>
         </div>
-        <div class="flex gap-8">
+        <div class="flex flex-col sm:flex-row gap-8">
           <NewProjectCard
             bg="grey"
             img="https://res.cloudinary.com/dkznczrj0/image/upload/v1750012052/Screenshot_2025-06-15_at_10.41.41_AM_lbwyhi.png"
@@ -36,15 +37,16 @@ import NewProjectCard from './NewProjectCard.vue'
         </div>
       </div>
 
+      <!-- capstones -->
       <div class="flex flex-col gap-12">
-        <div class="flex justify-between">
+        <div class="flex flex-col sm:flex-row gap-6 sm:gap-12 justify-between">
           <h2 class="text-left text-6xl font-bold">CAPSTONES</h2>
           <h3 class="text-xs text-right sm:text-base text-black/50">
             over the last few years, i've worked on three major development projects. <br />
             click on one to learn more.
           </h3>
         </div>
-        <div class="flex gap-8">
+        <div class="flex flex-col sm:flex-row gap-8">
           <NewProjectCard
             bg="blue"
             img="https://res.cloudinary.com/dkznczrj0/image/upload/v1742153378/reservoir-home_ayshfy.png"
@@ -66,15 +68,16 @@ import NewProjectCard from './NewProjectCard.vue'
         </div>
       </div>
 
+      <!-- design work -->
       <div class="flex flex-col gap-12 mb-24">
-        <div class="flex justify-between">
+        <div class="flex flex-col sm:flex-row gap-6 sm:gap-12 justify-between">
           <h2 class="text-left text-6xl font-bold">GRAPHIC DESIGN</h2>
           <h3 class="text-xs text-right sm:text-base text-black/50">
             during my time at florida southern, i worked on three major design projects. <br />
             click on one to learn more.
           </h3>
         </div>
-        <div class="grid grid-cols-3 gap-8">
+        <div class="flex flex-col sm:flex-row gap-8">
           <NewProjectCard
             bg="electricblue"
             img="https://res.cloudinary.com/dkznczrj0/image/upload/v1750015384/test-2_r6jdad.png"
