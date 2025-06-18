ed5069c89cb84f1ff66b242cd91475ca62ec59c1
Aiden Redmond <adnrdmnd@gmail.com>
2025-06-18 10:34:40 -0400

    links for james

--- src/views/NowView.vue
+++ src/views/NowView.vue
@@ -4,12 +4,24 @@
       <h1 class="text-4xl font-bold">what i'm loving</h1>
       <p class="mb-12 text-black/50">here's what i've been listening to, reading, and playing</p>
       <div class="columns-2 md:columns-3 lg:columns-3 gap-6 mt-4">
-        <img src="/current/bloom.webp" alt="" class="w-full mb-4 rounded-md" />
-        <img src="/current/vagabond.jpg" alt="" class="w-full mb-4 rounded-md" />
-        <img src="/current/doom.avif" alt="" class="w-full mb-4 rounded-md" />
-        <img src="/current/gnx.jpg" alt="" class="w-full mb-4 rounded-md" />
-        <img src="/current/dredge.jpg" alt="" class="w-full mb-4 rounded-md" />
-        <img src="/current/one-piece.webp" alt="" class="w-full mb-4 rounded-md" />
+        <a href="https://en.wikipedia.org/wiki/Bloom_(Beach_House_album)"
+          ><img src="/current/bloom.webp" alt="" class="w-full mb-4 rounded-md"
+        /></a>
+        <a href="https://en.wikipedia.org/wiki/Vagabond_(manga)" target="_blank"
+          ><img src="/current/vagabond.jpg" alt="" class="w-full mb-4 rounded-md"
+        /></a>
+        <a href="https://en.wikipedia.org/wiki/Doom:_The_Dark_Ages" target="_blank"
+          ><img src="/current/doom.avif" alt="" class="w-full mb-4 rounded-md"
+        /></a>
+        <a href="https://en.wikipedia.org/wiki/GNX_(album)" target="_blank"
+          ><img src="/current/gnx.jpg" alt="" class="w-full mb-4 rounded-md"
+        /></a>
+        <a href="https://en.wikipedia.org/wiki/Dredge_(video_game)" target="_blank"
+          ><img src="/current/dredge.jpg" alt="" class="w-full mb-4 rounded-md"
+        /></a>
+        <a href="https://en.wikipedia.org/wiki/One_Piece" target="_blank"
+          ><img src="/current/one-piece.webp" alt="" class="w-full mb-4 rounded-md"
+        /></a>
       </div>
     </div>
   </Transition>
