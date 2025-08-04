f3e32a61de8389160a220a2ef66fc70c09bdb972
Aiden Redmond <adnrdmnd@gmail.com>
2025-08-04 15:30:18 -0400

    photos page

---
+++ src/routes/photos/+page.svelte
@@ -0,0 +1,74 @@
+<div class="mb-12 grid grid-cols-1 gap-4 sm:mx-48 sm:grid-cols-2">
+	<div class="photo-col grid gap-4">
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1734363314/_MG_9430_lcteaz.jpg"
+				alt="broken CD"
+			/>
+		</div>
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1733704776/_MG_9121_rlj0zf.jpg"
+				alt="rachel in the garden"
+			/>
+		</div>
+	</div>
+	<div class="photo-col grid gap-4">
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1733704772/_MG_8206_tv3qin.jpg"
+				alt="anthony climbing an abandoned house"
+			/>
+		</div>
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1753920594/DSC03130_p02sjy.jpg"
+				alt="ponte vedra marsh at sunrise"
+			/>
+		</div>
+	</div>
+	<div class="photo-col grid gap-4">
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1747233445/kaws_r0i8v9.jpg"
+				alt="kaws"
+			/>
+		</div>
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1733780751/_MG_0884_cropped_fb9zep.jpg"
+				alt="elijah at the fire"
+			/>
+		</div>
+	</div>
+	<div class="photo-col grid gap-4">
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1733704773/_MG_8530_ibfvbq.jpg"
+				alt="lake mirror sculpture"
+			/>
+		</div>
+		<div>
+			<img
+				loading="lazy"
+				class="h-auto max-w-full rounded-xl transition-opacity duration-500"
+				src="https://res.cloudinary.com/dkznczrj0/image/upload/v1747233440/chris_ubi7of.jpg"
+				alt="chris at blue heat"
+			/>
+		</div>
+	</div>
+</div>
