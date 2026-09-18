1ea5625c90f99d02aa2604f9240d18ce07fbef05
Aiden Redmond <adnrdmnd@gmail.com>
2026-09-18 00:19:38 -0400

    format + simplify post display

--- src/routes/now/+layout.svelte
+++ src/routes/now/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
-  import { page } from '$app/state';
+	import { page } from '$app/state';
 	let { children, data } = $props();
 
-  let isHistorical = $derived(page.url.pathname.startsWith('/now/past'));
+	let isHistorical = $derived(page.url.pathname.startsWith('/now/past'));
 </script>
 
 <div class="flex gap-16">
@@ -11,19 +11,22 @@
 	</div>
 
 	<aside class="ml-32">
-  {#if !isHistorical}
-		<h2 class="text-black/50 mb-4">historical now pages</h2>
-		<ul class="space-y-2">
-			{#each data.historicalEntries as entry}
-				<li>
-					<a href="/now/past/{entry.slug}" class="underline">
-						{entry.metadata.date.slice(0,entry.metadata.date.indexOf("T"))}
-					</a>
-				</li>
-			{/each}
-		</ul>
-  {:else}
-      <p class="ml-[-40px] text-black/50">you're in the past. <br/> want to go back to the <a href="/now" class="underline">future?</a></p>
-  {/if}
+		{#if !isHistorical}
+			<h2 class="mb-4 text-black/50">historical now pages</h2>
+			<ul class="space-y-2">
+				{#each data.historicalEntries as entry}
+					<li>
+						<a href="/now/past/{entry.slug}" class="underline">
+							{entry.metadata.date.slice(0, entry.metadata.date.indexOf('T'))}
+						</a>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<p class="ml-[-40px] text-black/50">
+				you're in the past. <br /> want to go back to the
+				<a href="/now" class="underline">future?</a>
+			</p>
+		{/if}
 	</aside>
 </div>
--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,81 +1,82 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-  import type { SubstackPost } from './types.ts'
+	import type { SubstackPost } from './types.ts';
 
 	let posts: SubstackPost[] = $state([]);
 	let error = $state('');
 	let loading = $state(true);
 
-  let x = $state(0);
-  let y = $state(0);
-  let hoveredPost: SubstackPost | null = $state(null);
+	let x = $state(0);
+	let y = $state(0);
+	let hoveredPost: SubstackPost | null = $state(null);
 
-  function handleMouseMove(e: MouseEvent) {
-    x = e.clientX + 15;
-    y = e.clientY + 30;
-  }
+	function handleMouseMove(e: MouseEvent) {
+		x = e.clientX + 15;
+		y = e.clientY + 30;
+	}
 
-  async function fetchPosts() {
-    try {
-      const response = await fetch('api/writing');
-      const data = await response.json();
-      posts = data;
-    } catch (e) {
-      console.error(e);
-    }
-    loading = false;
-  }
+	async function fetchPosts() {
+		try {
+			const response = await fetch('api/writing');
+			const data = await response.json();
+			posts = data;
+		} catch (e) {
+			console.error(e);
+		}
+		loading = false;
+	}
 
-  onMount(fetchPosts);
+	onMount(fetchPosts);
 </script>
 
 <div id="latest-posts">
-  {#if loading}
-    <p>loading posts...</p>
-  {:else if error}
-    <p>{error}</p>
-  {:else if posts.length > 0}
-    <!-- <div class="mb-12">
+	{#if loading}
+		<p>loading posts...</p>
+	{:else if error}
+		<p>{error}</p>
+	{:else if posts.length > 0}
+		<!-- <div class="mb-12">
       <p>..incoming logs</p>
       <p>EVENT <span class="text-green bg-green/50">[{new Date(Date.now()).toISOString()}]</span> {posts.length} posts loaded from substack</p>
       <p>NOTICE <span class="text-green bg-green/50">[{new Date(Date.now() + (Math.random() * 1000)).toISOString()}]</span> click on a post that interests you to read it.</p>
     </div> -->
-    <div class="flex gap-2 flex-wrap text-4xl mb-32">
-      {#each posts as post}
-        <a
-          class="hover:bg-green/80"
-          href={post.canonical_url}
-          target="_blank"
-          onmouseenter={() => hoveredPost = post}
-          onmouseleave={() => hoveredPost = null}
-        >
-          <p>{post.title.toLowerCase()}</p>
-        </a>
-      {/each}
-    </div>
-  {:else}
-    <p>no posts were found...</p>
-  {/if}
+		<div class="mb-32 text-4xl leading-12">
+			{#each posts as post}
+				<a
+					class="hover:bg-green/80"
+					href={post.canonical_url}
+					target="_blank"
+					onmouseenter={() => (hoveredPost = post)}
+					onmouseleave={() => (hoveredPost = null)}
+				>
+					{post.title.toLowerCase()}
+				</a>
+				{' '}
+			{/each}
+		</div>
+	{:else}
+		<p>no posts were found...</p>
+	{/if}
 </div>
 
 {#if hoveredPost}
-  <img
-    src={hoveredPost.cover_image}
-    class="hover-image"
-    style="transform: translate({x}px, {y}px);"
-    alt={hoveredPost.title}
-  />
+	<img
+		src={hoveredPost.cover_image}
+		class="hover-image"
+		style="transform: translate({x}px, {y}px);"
+		alt={hoveredPost.title}
+	/>
 {/if}
 
 <svelte:window onmousemove={handleMouseMove} />
 
 <style>
-  .hover-image {
-    position: fixed;
-    top: 0;
-    left: 0;
-    pointer-events: none;
-    will-change: transform;
-    width: 300px;
-  }
+	.hover-image {
+		position: fixed;
+		top: 0;
+		left: 0;
+		pointer-events: none;
+		will-change: transform;
+		width: 300px;
+	}
 </style>
